b38ec449512757fabb827f63062de44f9c0851a9
ravisharma-09 <sharmaravi75080@gmail.com>
2026-05-02 20:39:59 +0530

    fecth input

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("hello")
+const passwordlength = document.getElementById("passwordlen")
+const submit = document.getElementsByClassName("submit")[0]
+const includelower = document.getElementById("lower")
+const includeupper = document.getElementById("Upper")
+
+const includenumber = document.getElementById("number")
+
+const includesymbol = document.getElementById("symbol")
+
+submit.addEventListener("click", () => {
+console.log(passwordlength.value)
+console.log(includelower.checked)
+console.log(includeupper.checked)
+console.log(includenumber.checked)
+console.log(includesymbol.checked)
+})
